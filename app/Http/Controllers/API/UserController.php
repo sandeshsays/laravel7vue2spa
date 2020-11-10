a7ab5f8114ef190232d0dddfa99c8da8991f201f
sandesh <sandeshsays@gmail.com>
2020-11-10 21:53:43 +0545

    auth:api implementation

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -10,6 +10,11 @@ use App\User;
 
 class UserController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
     /**
      * Display a listing of the resource.
      *
--- resources/js/router.js
+++ resources/js/router.js
@@ -5,6 +5,7 @@ Vue.use(VueRouter)
 let routes = [
     { path: '/dashboard', component: require('./components/Dashboard.vue').default},
     { path: '/users', component: require('./components/Users.vue').default},
+    { path: '/developer', component: require('./components/Developer.vue').default},
     { path: '/profile', component: require('./components/Profile.vue').default}
   ]
 
